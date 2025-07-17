0c6b6a95f2f20dd4889349c1b86506432fa79282
FrostyFire1 <kamilwielgomasit@gmail.com>
2025-07-17 14:10:00 +0200

    User/Role/Address structure

---
+++ src/main/java/com/frosty/SpringBootECommerce/model/Role.java
@@ -0,0 +1,20 @@
+package com.frosty.SpringBootECommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private AppRole role;
+}
---
+++ src/main/java/com/frosty/SpringBootECommerce/model/User.java
@@ -0,0 +1,38 @@
+package com.frosty.SpringBootECommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "store_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(min = 5, max = 32)
+    private String username;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 7, max = 128)
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
