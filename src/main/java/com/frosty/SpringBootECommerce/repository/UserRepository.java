91ba8dafbdbd0f57890948de975f04300ba42f98
FrostyFire1 <kamilwielgomasit@gmail.com>
2025-07-18 09:58:42 +0200

    custom UsreDetails and UserDetailsService

---
+++ src/main/java/com/frosty/SpringBootECommerce/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.frosty.SpringBootECommerce.repository;
+
+import com.frosty.SpringBootECommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/frosty/SpringBootECommerce/security/service/UserDetailsProvider.java
@@ -0,0 +1,86 @@
+package com.frosty.SpringBootECommerce.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.frosty.SpringBootECommerce.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsProvider implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsProvider from(User user){
+        List<SimpleGrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .toList();
+        return new UserDetailsProvider(user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        UserDetailsProvider that = (UserDetailsProvider) o;
+        return Objects.equals(this.id, that.id);
+    }
+}
