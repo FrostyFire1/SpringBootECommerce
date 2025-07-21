cd65a58829a29b333763cdfa0fa9ea54767d1f40
FrostyFire1 <kamilwielgomasit@gmail.com>
2025-07-21 17:36:03 +0200

    spotless (hi yannick)

--- src/main/java/com/frosty/SpringBootECommerce/DataInitializer.java
+++ src/main/java/com/frosty/SpringBootECommerce/DataInitializer.java
@@ -1,13 +1,8 @@
 package com.frosty.SpringBootECommerce;
 
-import com.frosty.SpringBootECommerce.model.Category;
-import com.frosty.SpringBootECommerce.model.Product;
 import com.frosty.SpringBootECommerce.repository.CategoryRepository;
 import com.frosty.SpringBootECommerce.repository.ProductRepository;
-import java.util.ArrayList;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,23 +12,23 @@ public class DataInitializer {
 
     private ProductRepository productRepository;
 
-//    @Bean
-//    public CommandLineRunner initDatabase() {
-//        return args -> {
-//            Category category = new Category(null, "Suits", new ArrayList<>());
-//            categoryRepository.save(category);
-//            productRepository.save(new Product(
-//                    null,
-//                    "Blue Suit No Tie",
-//                    "It's a suit without a tie (duh)",
-//                    11,
-//                    100.00,
-//                    10.0,
-//                    90.00,
-//                    "whatever",
-//                    category,
-//                    null,
-//                    null));
-//        };
-//    }
+    //    @Bean
+    //    public CommandLineRunner initDatabase() {
+    //        return args -> {
+    //            Category category = new Category(null, "Suits", new ArrayList<>());
+    //            categoryRepository.save(category);
+    //            productRepository.save(new Product(
+    //                    null,
+    //                    "Blue Suit No Tie",
+    //                    "It's a suit without a tie (duh)",
+    //                    11,
+    //                    100.00,
+    //                    10.0,
+    //                    90.00,
+    //                    "whatever",
+    //                    category,
+    //                    null,
+    //                    null));
+    //        };
+    //    }
 }
