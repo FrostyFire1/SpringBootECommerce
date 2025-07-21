8a831d157c09592114de8a85384e4646d2cfe114
FrostyFire1 <kamilwielgomasit@gmail.com>
2025-07-21 17:35:21 +0200

    Migrate to actual database from h2

--- src/main/java/com/frosty/SpringBootECommerce/DataInitializer.java
+++ src/main/java/com/frosty/SpringBootECommerce/DataInitializer.java
@@ -17,23 +17,23 @@ public class DataInitializer {
 
     private ProductRepository productRepository;
 
-    @Bean
-    public CommandLineRunner initDatabase() {
-        return args -> {
-            Category category = new Category(null, "Suits", new ArrayList<>());
-            categoryRepository.save(category);
-            productRepository.save(new Product(
-                    null,
-                    "Blue Suit No Tie",
-                    "It's a suit without a tie (duh)",
-                    11,
-                    100.00,
-                    10.0,
-                    90.00,
-                    "whatever",
-                    category,
-                    null,
-                    null));
-        };
-    }
+//    @Bean
+//    public CommandLineRunner initDatabase() {
+//        return args -> {
+//            Category category = new Category(null, "Suits", new ArrayList<>());
+//            categoryRepository.save(category);
+//            productRepository.save(new Product(
+//                    null,
+//                    "Blue Suit No Tie",
+//                    "It's a suit without a tie (duh)",
+//                    11,
+//                    100.00,
+//                    10.0,
+//                    90.00,
+//                    "whatever",
+//                    category,
+//                    null,
+//                    null));
+//        };
+//    }
 }
